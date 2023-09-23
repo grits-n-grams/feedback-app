994d75f429cc15aead642fb7e516c885bdfdf8db
gritsngrams <grits.n.grams@gmail.com>
2023-09-24 12:25:25 +1300

    add feedback list component & fetch feedback items

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
+const MAX_CHARS = 150;
 const textareaEl = document.querySelector(".form__textarea");
 const counterEl = document.querySelector(".counter");
 const formEl = document.querySelector(".form");
 const feedbackListEl = document.querySelector(".feedbacks");
 const submitBtnEl = document.querySelector(".submit-btn");
+const spinnerEl = document.querySelector(".spinner");
 
 //--counter component
 
 const inputHandler = () => {
-  const maxNumChars = 150;
   const numChars = textareaEl.value.length;
-  counterEl.textContent = maxNumChars - numChars;
+  counterEl.textContent = MAX_CHARS - numChars;
 };
 
 textareaEl.addEventListener("input", inputHandler);
 
-//--submit event
+//--Form Component
+
+const showVisualIndicator = (textCheck) => {
+  const className = textCheck === "valid" ? "form--valid" : "form--invalid";
+  formEl.classList.add(className);
+  setTimeout(() => {
+    formEl.classList.remove(className);
+  }, 2000);
+};
+
 const submitHandler = (e) => {
   e.preventDefault();
   const text = textareaEl.value;
   if (text.includes("#") && text.length > 4) {
-    formEl.classList.add("form--valid");
-    setTimeout(() => {
-      formEl.classList.remove("form--valid");
-    }, 2000);
+    showVisualIndicator("valid");
   } else {
-    formEl.classList.add("form--invalid");
-    setTimeout(() => {
-      formEl.classList.remove("form--invalid");
-    }, 2000);
+    showVisualIndicator("invalid");
     textareaEl.focus();
     return;
   }
-  const companyName = text
+
+  const company = text
     .split(" ")
     .find((word) => word.includes("#"))
     .substring(1);
-  const badgeLetter = companyName.substring(0, 1).toUpperCase();
+  const badgeLetter = company.substring(0, 1).toUpperCase();
   const upvoteCount = 0;
   const daysAgo = 0;
   const feedBackItemHtml = `
@@ -48,7 +53,7 @@ const submitHandler = (e) => {
       <p class="feedback__letter">${badgeLetter}</p>
   </section>
   <div class="feedback__content">
-      <p class="feedback__company">${companyName}</p>
+      <p class="feedback__company">${company}</p>
       <p class="feedback__text">${text}</p>
   </div>
   <p class="feedback__date">${daysAgo === 0 ? "new" : `${daysAgo}d`}</p>
@@ -56,10 +61,41 @@ const submitHandler = (e) => {
   `;
 
   feedbackListEl.insertAdjacentHTML("beforeend", feedBackItemHtml);
-
   textareaEl.value = "";
   submitBtnEl.blur();
-  counterEl.textContent = "150";
+  counterEl.textContent = MAX_CHARS;
 };
 
 formEl.addEventListener("submit", submitHandler);
+
+//-- FEEDBACK LIST COMPONENT
+
+fetch("https://bytegrad.com/course-assets/js/1/api/feedbacks")
+  .then((res) => res.json())
+  .then((data) => {
+    spinnerEl.remove();
+    data.feedbacks.forEach((feedback) => {
+      const feedBackItemHtml = `
+        <li class="feedback">
+        <button class="upvote">
+            <i class="fa-solid fa-caret-up upvote__icon"></i>
+            <span class="upvote__count">${feedback.upvoteCount}</span>
+        </button>
+        <section class="feedback__badge">
+            <p class="feedback__letter">${feedback.badgeLetter}</p>
+        </section>
+        <div class="feedback__content">
+            <p class="feedback__company">${feedback.company}</p>
+            <p class="feedback__text">${feedback.text}</p>
+        </div>
+        <p class="feedback__date">${
+          feedback.daysAgo === 0 ? "new" : `${feedback.daysAgo}d`
+        }</p>
+      </li>
+        `;
+      feedbackListEl.insertAdjacentHTML("beforeend", feedBackItemHtml);
+    });
+  })
+  .catch((error) => {
+    feedbackListEl.textContent = `Failed to fetch feedback items: ${error.message}`;
+  });

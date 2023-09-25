92df87c3be8c7595400c3118ebf61ed7f854ce0d
gritsngrams <grits.n.grams@gmail.com>
2023-09-25 17:34:12 +1300

    added POST request & refactored

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const MAX_CHARS = 150;
+const BASE_API_URL = "https://bytegrad.com/course-assets/js/1/api";
 const textareaEl = document.querySelector(".form__textarea");
 const counterEl = document.querySelector(".counter");
 const formEl = document.querySelector(".form");
 const feedbackListEl = document.querySelector(".feedbacks");
 const submitBtnEl = document.querySelector(".submit-btn");
 const spinnerEl = document.querySelector(".spinner");
+
+const renderFeedbackItem = (feedbackItem) => {
+  const feedBackItemHtml = `
+  <li class="feedback">
+  <button class="upvote">
+      <i class="fa-solid fa-caret-up upvote__icon"></i>
+      <span class="upvote__count">${feedbackItem.upvoteCount}</span>
+  </button>
+  <section class="feedback__badge">
+      <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
+  </section>
+  <div class="feedback__content">
+      <p class="feedback__company">${feedbackItem.company}</p>
+      <p class="feedback__text">${feedbackItem.text}</p>
+  </div>
+  <p class="feedback__date">${
+    feedbackItem.daysAgo === 0 ? "new" : `${feedbackItem.daysAgo}d`
+  }</p>
+</li>
+  `;
+
+  feedbackListEl.insertAdjacentHTML("beforeend", feedBackItemHtml);
+};
 
 //--counter component
 
@@ -43,24 +67,37 @@ const submitHandler = (e) => {
   const badgeLetter = company.substring(0, 1).toUpperCase();
   const upvoteCount = 0;
   const daysAgo = 0;
-  const feedBackItemHtml = `
-  <li class="feedback">
-  <button class="upvote">
-      <i class="fa-solid fa-caret-up upvote__icon"></i>
-      <span class="upvote__count">${upvoteCount}</span>
-  </button>
-  <section class="feedback__badge">
-      <p class="feedback__letter">${badgeLetter}</p>
-  </section>
-  <div class="feedback__content">
-      <p class="feedback__company">${company}</p>
-      <p class="feedback__text">${text}</p>
-  </div>
-  <p class="feedback__date">${daysAgo === 0 ? "new" : `${daysAgo}d`}</p>
-</li>
-  `;
 
-  feedbackListEl.insertAdjacentHTML("beforeend", feedBackItemHtml);
+  const feedbackItem = {
+    upvoteCount,
+    badgeLetter,
+    daysAgo,
+    company,
+    text,
+  };
+
+  renderFeedbackItem(feedbackItem);
+
+  //--send to server
+  fetch(`${BASE_API_URL}/feedbacks`, {
+    method: "POST",
+    body: JSON.stringify(feedbackItem),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        console.log("something went wrong");
+        return;
+      }
+      console.log("success");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
   textareaEl.value = "";
   submitBtnEl.blur();
   counterEl.textContent = MAX_CHARS;
@@ -70,30 +107,12 @@ formEl.addEventListener("submit", submitHandler);
 
 //-- FEEDBACK LIST COMPONENT
 
-fetch("https://bytegrad.com/course-assets/js/1/api/feedbacks")
+fetch(`${BASE_API_URL}/feedbacks`)
   .then((res) => res.json())
   .then((data) => {
     spinnerEl.remove();
     data.feedbacks.forEach((feedback) => {
-      const feedBackItemHtml = `
-        <li class="feedback">
-        <button class="upvote">
-            <i class="fa-solid fa-caret-up upvote__icon"></i>
-            <span class="upvote__count">${feedback.upvoteCount}</span>
-        </button>
-        <section class="feedback__badge">
-            <p class="feedback__letter">${feedback.badgeLetter}</p>
-        </section>
-        <div class="feedback__content">
-            <p class="feedback__company">${feedback.company}</p>
-            <p class="feedback__text">${feedback.text}</p>
-        </div>
-        <p class="feedback__date">${
-          feedback.daysAgo === 0 ? "new" : `${feedback.daysAgo}d`
-        }</p>
-      </li>
-        `;
-      feedbackListEl.insertAdjacentHTML("beforeend", feedBackItemHtml);
+      renderFeedbackItem(feedback);
     });
   })
   .catch((error) => {
